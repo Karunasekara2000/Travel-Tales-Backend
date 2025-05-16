6a625b0c83be1c3bab8abce154af4ba235a7ade6
lahiruSandaruwan20 <lahiru.20210390@iit.ac.lk>
2025-05-17 00:51:09 +0530

    implemented post service with logs.

--- src/service/postService.js
+++ src/service/postService.js
@@ -1,63 +1,91 @@
-// /backend/services/postService.js
 const postDao = require('../dao/postDao');
+const logService = require('../service/logService');
+const userDao = require("../controller/postController");
 
 async function listPosts(filterOpts) {
     return postDao.getPosts(filterOpts);
 }
 
 async function getPost(id) {
-    return postDao.getPostById(id);
+    const posts = await postDao.getPosts(id);
+    await logService.log(posts.authorId, "GET_POSTS", posts.id, `Get posts by author id '${posts.title}'`);
+    return posts;
 }
 
 async function createPost(userId, postData) {
-    return postDao.createPost({ ...postData, authorId: userId });
+    const post = await postDao.createPost({ ...postData, authorId: userId });
+    await logService.log(userId, "POST_CREATE", post.id, `Created post titled '${postData.title}'`);
+    return post;
 }
 
 async function updatePost(id, userId, postData) {
-    return postDao.updatePost(id, userId, postData);
+    const updated = await postDao.updatePost(id, userId, postData);
+    await logService.log(userId, "POST_UPDATE", id, `Updated post titled '${postData.title}'`);
+    return updated;
 }
 
 async function deletePost(id, userId) {
-    return postDao.deletePost(id, userId);
+    const deleted = await postDao.deletePost(id, userId);
+    await logService.log(userId, "POST_DELETE", id, "Deleted post");
+    return deleted;
 }
 
-// --- Likes ---
 async function likeOrDislikePost(userId, postId, isLike) {
-    return postDao.likePost(userId, postId, isLike);
+    const result = await postDao.likePost(userId, postId, isLike);
+    const action = isLike ? "LIKE" : "DISLIKE";
+    await logService.log(userId, `POST_${action}`, postId, `${action}d a post`);
+    return result;
 }
 
 async function removeLikeOrDislike(userId, postId) {
-    return postDao.unlikePost(userId, postId);
+    const result = await postDao.unlikePost(userId, postId);
+    await logService.log(userId, "POST_LIKE_REMOVE", postId, "Removed like/dislike");
+    return result;
 }
 
-// --- Follows ---
 async function followUser(followerId, followingId) {
-    return postDao.followUser(followerId, followingId);
+    const result = await postDao.followUser(followerId, followingId);
+    await logService.log(followerId, "FOLLOW_USER", followingId, "Started following user");
+    return result;
 }
 
 async function unfollowUser(followerId, followingId) {
-    return postDao.unfollowUser(followerId, followingId);
+    const result = await postDao.unfollowUser(followerId, followingId);
+    await logService.log(followerId, "UNFOLLOW_USER", followingId, "Stopped following user");
+    return result;
 }
 
 async function getUserFollowers(userId) {
-    return postDao.getFollowers(userId);
+    const followers = await postDao.getFollowers(userId);
+    await logService.log(userId, "GET_FOLLOWERS", followers.id, "Get followers");
+
+    return followers;
 }
 
 async function getUserFollowing(userId) {
-    return postDao.getFollowing(userId);
+    const followings = await postDao.getFollowing(userId);
+    await logService.log(userId, "GET_FOLLOWINGS", followings.id, "Get followings");
+    return followings;
 }
 
-// --- Comments ---
+
 async function fetchComments(postId) {
-    return postDao.getCommentsByPost(postId);
+
+    const comments = postDao.getCommentsByPost(postId)
+    await logService.log(comments.userId, "GET_COMMENTS", comments.id, "Get Comments");
+    return comments;
 }
 
 async function createComment(postId, userId, comment) {
-    return postDao.addComment(postId, userId, comment);
+    const result = await postDao.addComment(postId, userId, comment);
+    await logService.log(userId, "COMMENT_CREATE", postId, "Added a comment");
+    return result;
 }
 
 async function removeComment(commentId, userId) {
-    return postDao.deleteComment(commentId, userId);
+    const result = await postDao.deleteComment(commentId, userId);
+    await logService.log(userId, "COMMENT_DELETE", commentId, "Deleted a comment");
+    return result;
 }
 
 module.exports = {
